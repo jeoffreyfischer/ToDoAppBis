667c07dd60a3537d570e254cca0540bdf7e4c8c5
Jeoffrey Fischer <jeoffrey.fischer@gmail.com>
2023-08-10 15:13:53 +1000

    API-linked-to-databse-with-initial-migration

---
+++ todo-api/Migrations/20230810050633_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace todo_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsComplete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "IsComplete", "Name" },
+                values: new object[,]
+                {
+                    { 1L, true, "Walk dog" },
+                    { 2L, false, "Go grocery shopping" },
+                    { 3L, false, "Eat lunch" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
